8bc945a25631f82813f54d186d7500884ba0874a
Tejas Kedare <tejaskedare2@gmail.com>
2020-08-30 19:21:19 +0530

    broken our homepage into smaller components, finished styling meu-item component

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../components/directory/directory.components";
+import '../pages/homepage.styles.scss'
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
